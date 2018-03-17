e187e31ebe9e948cb7eda3b0328a05232d036cd0
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2018-03-17 18:12:06 +0100

    Make ControlCombo intercept input for buttons

--- FerranXone.js
+++ FerranXone.js
@@ -8,16 +8,25 @@ midi.off = 0x80;
 midi.keyPressure = 0xA0;
 midi.kp = 0xA0;
 midi.controlChange = 0xB0;
-midi.cc == 0xB0;
+midi.cc = 0xB0;
 
-XoneChain.controlComboGroup = new controlcombo.ControlComboGroup({numberOfControlCombos: 4});
+XoneChain.controlComboGroup = new controlcombo.ControlComboGroup({
+    numberOfControlCombos: 4,
+    components: {
+        sillyButton: new components.Button({
+            input: function (channel, control, value, status, group) {
+                print("button do something");
+            }
+        })
+    }
+});
 
 XoneChain.mapping = new mapper.MidiMapper();
 
 XoneChain.init = function() {
     components.Button.prototype.isPress = function (channel, control, value, status) {
         return (status & 0xF0) === midi.noteOn;
-    }
+    };
 
     XoneChain.mapping.init();
 };
@@ -25,14 +34,15 @@ XoneChain.init = function() {
 
 // Mapping
 // =======
+var valueOff = controllers.spectra.off;
 
 XoneChain.mapping.mapControlComboShift = function(parameters) {
     var i = parameters.i;
     var channel = parameters.channel;
     var control = parameters.control;
-    var onValue = controllers.spectra.pink;
-    var loadedValue = controllers.spectra.dimTeal;
-    var offValue = controllers.spectra.off;
+
+    var valueOn = controllers.spectra.pink;
+    var valueLoaded = controllers.spectra.dimTeal;
 
     XoneChain.mapping.map(
         channel,
@@ -46,18 +56,57 @@ XoneChain.mapping.mapControlComboShift = function(parameters) {
         channel + midi.noteOn,
         control
     ];
-    XoneChain.controlComboGroup[i].shiftButton.onValue = onValue;
-    XoneChain.controlComboGroup[i].shiftButton.loadedValue = loadedValue;
-    XoneChain.controlComboGroup[i].shiftButton.offValue = offValue;
+    XoneChain.controlComboGroup[i].shiftButton.valueOn = valueOn;
+    XoneChain.controlComboGroup[i].shiftButton.valueLoaded = valueLoaded;
+    XoneChain.controlComboGroup[i].shiftButton.valueOff = valueOff;
+};
+
+XoneChain.mapping.mapControlComboTrigger = function(parameters) {
+    var i = parameters.i;
+    var channel = parameters.channel;
+    var control = parameters.control;
+
+    var valueOn = controllers.spectra.red;
+    var valueLoaded = controllers.spectra.dimRed;
+
+    XoneChain.mapping.map(
+        channel,
+        control,
+        "all",
+        function(channel, control, value, status, group) {
+            XoneChain.controlComboGroup[i].triggerButton.input(channel, control, value, status, group);
+        }
+    );
+    XoneChain.controlComboGroup[i].triggerButton.midi = [
+        channel + midi.noteOn,
+        control
+    ];
+    XoneChain.controlComboGroup[i].triggerButton.valueOn = valueOn;
+    XoneChain.controlComboGroup[i].triggerButton.valueLoaded = valueLoaded;
+    XoneChain.controlComboGroup[i].triggerButton.valueOff = valueOff;
 };
 
 XoneChain.mapping.init = function() {
+
+    XoneChain.mapping.map(
+        controllers.spectra.channel,
+        controllers.spectra[1][0],
+        "all",
+        function(channel, control, value, status, group) {
+            XoneChain.controlComboGroup.components.sillyButton.input(channel, control, value, status, group);
+        }
+    );
 
     // ControlCombo 1
     XoneChain.mapping.mapControlComboShift({
         i:          0,
         channel:    controllers.spectra.channel,
         control:    controllers.spectra[2][0],
+    });
+    XoneChain.mapping.mapControlComboTrigger({
+        i:          0,
+        channel:    controllers.spectra.channel,
+        control:    controllers.spectra[3][0],
     });
 
     // ControlCombo 2
@@ -66,6 +115,11 @@ XoneChain.mapping.init = function() {
         channel:    controllers.spectra.channel,
         control:    controllers.spectra[2][1],
     });
+    XoneChain.mapping.mapControlComboTrigger({
+        i:          1,
+        channel:    controllers.spectra.channel,
+        control:    controllers.spectra[3][1],
+    });
 
     // ControlCombo 3
     XoneChain.mapping.mapControlComboShift({
@@ -73,12 +127,22 @@ XoneChain.mapping.init = function() {
         channel:    controllers.spectra.channel,
         control:    controllers.spectra[2][2],
     });
+    XoneChain.mapping.mapControlComboTrigger({
+        i:          2,
+        channel:    controllers.spectra.channel,
+        control:    controllers.spectra[3][2],
+    });
 
     // ControlCombo 4
     XoneChain.mapping.mapControlComboShift({
         i:          3,
         channel:    controllers.spectra.channel,
         control:    controllers.spectra[2][3],
+    });
+    XoneChain.mapping.mapControlComboTrigger({
+        i:          3,
+        channel:    controllers.spectra.channel,
+        control:    controllers.spectra[3][3],
     });
 
 };
--- common-controlcombo.js
+++ common-controlcombo.js
@@ -6,93 +6,140 @@
         return temp;
     }
 
-    var state = {
-        on: 1,
-        off: 0
-    }
+    // TODO: proper prototyping and constructor in components
 
-    // TODO
-    // components.ComponentContainer.prototype.constructor = ComponentContainer;
+    // TODO: full lodash build for mixxx
 
     /**
-     * ControlComboShiftButton
+     * ControlComboButton
      * @param {Object} properties
      * @param {ControlCombo} properties.controlCombo - The ControlCombo that holds this button,
-     * @param {function} properties.onValue - The midi value to send when the button is pressed.
-     * @param {function} properties.loadedValue - The midi value to send when the button is not pressed but its ControlCombo has actions assigned.
-     * @param {function} properties.offValue - The midi value to send when the button is not pressed and its ControlCombo has no actions assigned.
+     * @param {function} properties.onPress - Function to call when the button is pressed.
+     * @param {function} properties.onRelease - Function to call when the button is released.
+     * @param {number} [properties.valueOn=127] - The midi value to send when the button is pressed.
+     * @param {number} [properties.valueLoaded=64] - The midi value to send when the button is not pressed but its ControlCombo has actions assigned.
+     * @param {number} [properties.valueOff=0] - The midi value to send when the button is not pressed and its ControlCombo has no actions assigned.
      * @constructor
      */
-    var ControlComboShiftButton = function(properties) {
+    var ControlComboButton = function(properties) {
 
-        this.state = state.off;
-        this.onValue = 127;
-        this.loadedValue = 64;
-        this.offValue = 0;
+        this.isOn = false;
+        this.valueOn = 127;
+        this.valueLoaded = 64;
+        this.valueOff = 0;
+        this.onPress = function () {};
+        this.onRelease = function () {};
 
         components.Button.call(this, properties);
     };
 
-    ControlComboShiftButton.prototype = _.create(components.Button.prototype, {
+    ControlComboButton.prototype = _.create(components.Button.prototype, {
 
-        constructor: ControlComboShiftButton,
+        constructor: ControlComboButton,
 
         inSetValue: function(isPress) {
             if (isPress) {
-                this.state = state.on;
-                this.controlCombo.shiftPressed();
+                this.isOn = true;
+                this.onPress();
                 this.sendOutput();
             } else {
-                this.state = state.off;
-                this.controlCombo.shiftReleased();
+                this.isOn = false;
+                this.onRelease();
                 this.sendOutput();
             }
         },
 
+        sendOutPut: function() {}
+    });
+
+
+    /**
+     * ControlComboShiftButton
+     * @param {Object} properties
+     * @constructor
+     */
+    var ControlComboShiftButton = function(properties) {
+
+        ControlComboButton.call(this, properties);
+    };
+
+    ControlComboShiftButton.prototype = _.create(ControlComboButton.prototype, {
+
+        constructor: ControlComboShiftButton,
+
         sendOutput: function () {
-            if (this.state === state.on) {
-                this.send(this.onValue);
+            if (this.isOn === true) {
+                this.send(this.valueOn);
             } else if (this.controlCombo.isLoaded) {
-                this.send(this.loadedValue);
+                this.send(this.valueLoaded);
             } else {
-                this.send(this.offValue);
+                this.send(this.valueOff);
             }
         },
 
         unselect: function () {
-            this.state = state.off;
+            this.isOn = false;
             this.sendOutput();
         }
     });
 
 
+    /**
+     * ControlComboTriggerButton
+     * @param {Object} properties
+     * @constructor
+     */
+    var ControlComboTriggerButton = function(properties) {
 
-    // ControlComboTriggerButton
-    // =======================
-    var ControlComboTriggerButton = function(controlCombo) {
+        ControlComboButton.call(this, properties);
 
-        this.controlCombo = controlCombo;
+        this.animationStep = 0;
+        this.timer = null;
     };
-    
-    ControlComboTriggerButton.prototype = _.create(components.Button.prototype, {
+
+    ControlComboTriggerButton.prototype = _.create(ControlComboButton.prototype, {
 
         constructor: ControlComboTriggerButton,
 
-        inSetValue: function(isPress) {
-            if (isPress) {
-                this.controlCombo.triggerPressed();
-                this.output(1);
-            } else {
-                this.controlCombo.triggerReleased();
-                this.output(0);
+        sendOutput: function () {
+
+        },
+
+        animate: function() {
+            if (this.timer === null) {
+                this.animation();
             }
         },
 
-        outValueScale: function(value) {
-            return value * (this.max - this.min) + this.min;
-        },
+        // TODO: encapsulate in TimedSequence class or something like that
+        animation: function() {
+            var self = this;
+            if (self.animationStep == 0 && self.timer === null) {
+                self.timer = engine.beginTimer(25, function() {self.animation()});
+                this.send(controllers.spectra.yellow);
+                self.animationStep += 1;
+            } else if (self.animationStep == 1) {
+                this.send(this.valueOff);
+                self.animationStep += 1;
+            } else if (self.animationStep == 2) {
+                this.send(controllers.spectra.red);
+                self.animationStep += 1;
+            } else if (self.animationStep == 3) {
+                this.send(this.valueOff);
+                self.animationStep += 1;
+            } else if (self.animationStep == 4) {
+                this.send(controllers.spectra.teal);
+                self.animationStep += 1;
+            } else if (self.animationStep == 5) {
+                this.send(this.valueOff);
+                if (self.timer !== null) {
+                    engine.stopTimer(self.timer);
+                    self.timer = null;
+                }
+                self.animationStep = 0;
+            }
+        }
     });
-
 
 
     /**
@@ -104,54 +151,98 @@
      */
     var ControlCombo = function(properties) {
 
-        this.shiftButton = new ControlComboShiftButton({controlCombo: this});
-        this.triggerButton = new ControlComboTriggerButton({controlCombo: this});
-        this.isLoaded = false;
+        var self = this;
 
-        components.ComponentContainer.call(this, properties);
+        _.assign(self, properties);
+
+        self.shiftButton = new ControlComboShiftButton({
+            controlCombo: self,
+            onPress: function () { self.shiftPressed(); },
+            onRelease: function () { self.shiftReleased(); }
+        });
+        self.triggerButton = new ControlComboTriggerButton({
+            controlCombo: self,
+            onPress: function () { self.triggerPressed(); },
+        });
+        self.isLoaded = false;
+        self.shiftOn = false;
     };
 
-    ControlCombo.prototype = _.create(components.ComponentContainer.prototype, {
-
-        constructor: ControlCombo,
+    ControlCombo.prototype = {
 
         shiftPressed: function () {
+            this.shiftOn = true;
             this.controlComboGroup.controlComboSelected(this.id);
         },
 
         shiftReleased: function () {
+            this.shiftOn = false;
             this.controlComboGroup.controlComboUnSelected(this.id);
+        },
+
+        triggerPressed: function () {
+            if (this.triggerButton.isOn === true &&
+                !this.shiftOn && this.isLoaded) {
+                this.triggerButton.animate();
+                this.isLoaded = false;
+                this.shiftButton.sendOutput();
+            }
         },
 
         unselect: function () {
             this.shiftButton.unselect();
+        },
+
+        captureMessage: function (component, channel, control, value, status, group) {
+            print("capture");
+            this.isLoaded = true;
         }
-    });
+    };
 
 
     /**
      * ControlComboGroup
      * @param {Object} properties
      * @param {number} properties.numberOfControlCombos - Number of ControlCombos that will make up the ControlComboGroup.
+     * @param {Object} properties.components
      * @constructor
      */
     var ControlComboGroup = function(properties) {
 
-        this.numberOfControlCombos = properties.numberOfControlCombos;
+        var self = this;
+
+        _.assign(self, properties);
+        self.components = new components.ComponentContainer(properties.components);
+
+        for (var id = 0; id < self.numberOfControlCombos; ++id) {
     
-        for (var id = 0; id < this.numberOfControlCombos; ++id) {
-    
-            this[id] = new ControlCombo({
-    
-                controlComboGroup: this,
+            self[id] = new ControlCombo({
+                controlComboGroup: self,
                 id: id,
-    
             });
         }
+
+        // Set self as proxy to the buttons input
+        this.components.forEachComponent(function (component) {
+            if (component instanceof components.Button) {
+                var componentInput = component.input;
+                // TODO: overwriting the input function is dangerous because other libraries might do the same
+                component.input = function (channel, control, value, status, group) {
+                    if (self.activeShiftButton !== null) {
+                        self.captureMessage(component, channel, control, value, status, group);
+                    } else {
+                        componentInput.call(component, channel, control, value, status, group);
+                    }
+                }
+            }
+            // && (component.type === Button.prototype.types.push
+            //     || component.type === undefined)
+            // && component.input === Button.prototype.input
+        }, true);
     
-        this.activeShiftButton = null;
+        self.activeShiftButton = null;
     };
-    
+
     ControlComboGroup.prototype = {
 
         controlComboSelected: function(id) {
@@ -169,11 +260,17 @@
                 this.activeShiftButton = null;
             }
         },
+
+        captureMessage: function(component, channel, control, value, status, group) {
+            if (this.activeShiftButton == null) { return; }
+
+            this[this.activeShiftButton].captureMessage(component, channel, control, value, status, group);
+
+        }
     };
 
     var exports = {};
-    exports.ControlComboShiftButton = ControlComboShiftButton;
-    exports.ControlComboTriggerButton = ControlComboTriggerButton;
+    exports.ControlComboButton = ControlComboShiftButton;
     exports.ControlCombo = ControlCombo;
     exports.ControlComboGroup = ControlComboGroup;
     global.controlcombo = exports;
